1b86a3c23b09b4388e8b0052a584cb8f09449aa3
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2013-04-20 10:47:33 +0430

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kelasi::Application.config.secret_token = '9bdc2ee1d230d9f4a8f318e0b8439ecefcbfe32e853c2582143d414f55bebfd72bd70126ec7490786dc899fdbb6d5d51c815846d5cd42ee70f2224349311493c'
