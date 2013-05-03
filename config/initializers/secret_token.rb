01c6c98406ee99de7c1ec74a0ad745a00ce1f749
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2013-05-03 15:36:59 +0430

    Updated to rails v4. Also Gemfile source has changed to use HTTP instead of HTTPS

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Kelasi::Application.config.secret_token = '9bdc2ee1d230d9f4a8f318e0b8439ecefcbfe32e853c2582143d414f55bebfd72bd70126ec7490786dc899fdbb6d5d51c815846d5cd42ee70f2224349311493c'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kelasi::Application.config.secret_key_base = '5f02e57a65db780831938693b01a7c7aeac4fac91a90bed15bf45b8d4c461a06311f4b27a3a59d7e2167f679cb490a55c5da72b0c7f7072ddaf6e83a376a9578'
