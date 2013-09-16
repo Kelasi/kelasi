44b6f7c6fb91466d08c1a453425c85bb1550ea29
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2013-09-16 17:27:05 +0430

    Added university resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Kelasi::Application.routes.draw do
+  resources :universities
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130819120713) do
+ActiveRecord::Schema.define(version: 20130916125042) do
+
+  create_table "universities", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
