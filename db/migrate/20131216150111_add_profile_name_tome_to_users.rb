01804ec9f1996231f579efe1ce8035e7d78d4151
saeedSarpas <s.sarpas@gmail.com>
2013-12-18 22:49:47 +0330

    Added profile_name to User model

---
+++ db/migrate/20131216150111_add_profile_name_tome_to_users.rb
@@ -0,0 +1,6 @@
+class AddProfileNameTomeToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :profile_name, :string
+    add_index :users, :profile_name, :unique => true
+  end
+end
