d7e554b041c80c9be65e44f9c8baa87e1fba25af
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2014-03-08 18:10:19 +0330

    Added some utility methods to Timeline model

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -18,4 +18,14 @@ class Timeline < ActiveRecord::Base
     return true if self.timeline_user_permissions.where(role: TimelineUserPermission::Roles::ADMIN
                                                        ).map(&:user).include? user
   end
+
+  def add_member(user, options={})
+    role = options[:role] || TimelineUserPermission::Roles::PARTICIPANTS
+    raise "expecting an instance of User" unless user.instance_of? User
+    self.timeline_user_permissions.create! user: user, role: role
+  end
+
+  def role?(user)
+    timeline_user_permissions.find_by(user: user).role
+  end
 end
