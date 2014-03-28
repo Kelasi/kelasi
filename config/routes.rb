f63340bca27e2bbbcffdb2f0f0260924abbb3f54
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2014-03-28 19:03:43 +0430

    Removed some stuff related to frontend and fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -18,57 +18,5 @@ Kelasi::Application.routes.draw do
     get '/stream', to: 'streams#index'
   end
 
-  scope '/fe_', module: :frontend do
-    get '/(*page)', to: 'pages#page'
-  end
-
-  root to: 'frontend/pages#page'
-  get '/*path', to: 'frontend/pages#page'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get '*path' => redirect('/')
 end
