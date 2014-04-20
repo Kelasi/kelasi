e5904e8f3319b91411a093e36754060682566748
saeedSarpas <s.sarpas@gmail.com>
2014-04-20 22:36:29 +0430

    Updated some files which Travic CI be able to run tests
    travis.yml: waiting for elasticsearch to start
    gitmodules: change the git repo address to https
    spec_helper: allow webmock net connection through localhost

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,18 @@
 require 'rubygems'
 
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+
+require 'webmock/rspec'
+WebMock.disable_net_connect!(:allow_localhost => true)
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
+#
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
