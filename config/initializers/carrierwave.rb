144442830821f0ea2ddf5d887e6b7c49dbb692b6
saeedSarpas <s.sarpas@gmail.com>
2014-04-22 01:40:02 +0430

    Initialized CarrierWave

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,14 @@
+if Rails.env.test?
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+    config.root = "#{Rails.root}/storage/spec_files"
+  end
+else
+  CarrierWave.configure do |config|
+    config.permissions = 0600
+    config.directory_permissions = 0700
+    config.storage = :file
+    config.root = "#{Rails.root}/storage"
+  end
+end
