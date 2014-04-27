47c0beb99d869d902d88148722cd970ff1301f16
saeedSarpas <s.sarpas@gmail.com>
2014-04-27 16:23:33 +0430

    Added minimagick to Gemfile and updated travis pre-install configs

--- Gemfile
+++ Gemfile
@@ -47,3 +47,4 @@ gem 'tire'
 gem 'kaminari'
 
 gem 'carrierwave'
+gem "mini_magick"
--- app/uploaders/medium_uploader.rb
+++ app/uploaders/medium_uploader.rb
@@ -9,7 +9,7 @@ class MediumUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
